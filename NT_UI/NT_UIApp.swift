c0a432aef95bcd86cedc7a9ba3259cb5a4fe55a2
JJ <Vmefifty@gmail.com>
2026-05-27 21:41:53 +0800

    Initial Commit

---
+++ NT_UI/NT_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NT_UIApp.swift
+//  NT_UI
+//
+//  Created by 蒋棕基 on 2026/5/27.
+//
+
+import SwiftUI
+
+@main
+struct NT_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
